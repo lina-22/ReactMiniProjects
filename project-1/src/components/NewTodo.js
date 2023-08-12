57d30416555dead75a9f936f055897976cd03b06
lina-22 <haquefahinur@gmail.com>
2023-08-12 23:10:19 +0200

    contolled components and getting form value

--- project-1/src/components/NewTodo.js
+++ project-1/src/components/NewTodo.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./newtodo.module.css";
 
 export default function NewTodo() {
+  const [todo, setTodo] = useState({ title: "", desc: "" });
+  const { title, desc } = todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+    setTodo({ title: "", desc: "" });
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    setTodo((oldTodo) => {
+      return { ...oldTodo, [name]: e.target.value };
+    });
+  };
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <div className={style["form-field"]}>
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
       </div>
       <div className={style["form-field"]}>
         <label htmlFor="desc">Description:</label>
-        <textarea type="text" id="desc" name="desc" />
+        <textarea
+          type="text"
+          id="desc"
+          name="desc"
+          value={desc}
+          onChange={handleChange}
+        />
       </div>
       <button type="submit">Add todo</button>
     </form>
