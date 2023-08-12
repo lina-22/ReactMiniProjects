502854aeb7f4133abf5bae90f4059cd521e00c51
lina-22 <haquefahinur@gmail.com>
2023-08-12 22:16:37 +0200

    style added

--- project-1/src/components/Todo.js
+++ project-1/src/components/Todo.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import style from "./todo.module.css";
 export default function Todo(props) {
   const { title, desc, id } = props.todo;
   return (
-    <article>
+    <article className={style.todo}>
       <div>
         <h3>{title}</h3>
         <p>{desc}</p>
       </div>
-      <button>
+      <button className={style.btn}>
         <i className="fa fa-trash fa-2x"></i>
       </button>
     </article>
--- project-1/src/components/Todos.js
+++ project-1/src/components/Todos.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Todo from "./Todo";
+import style from "./todos.module.css";
 
 export default function Todos(props) {
   const { todos } = props;
 
   return (
     <div>
-      <section>
+      <section className={style.todos}>
         <article>
           {todos.map((todo) => (
             <Todo key={todo.id} todo={todo} />
