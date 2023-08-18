57aeca511bd03f428a33ae55924309f3aab61339
lina-22 <haquefahinur@gmail.com>
2023-08-18 21:15:33 +0200

    project-3 delete country

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState(countries);
 
   const fetchData = async (url) => {
     setIsLoading(true);
@@ -14,6 +15,7 @@ function App() {
       const response = await fetch(url);
       const data = await response.json();
       setCountries(data);
+      setFilteredCountries(data);
       setIsLoading(false);
       setError(null);
       // console.log(countries);
@@ -26,12 +28,23 @@ function App() {
   useEffect(() => {
     fetchData(url);
   }, []);
+  const handleRemoveCountry = (name) => {
+    const filter = filteredCountries.filter(
+      (country) => country.name.common !== name
+    );
+    setFilteredCountries(filter);
+  };
 
   return (
     <>
       <h1>Country App</h1>;{isLoading && <h2>Loading....</h2>}
       {error && <h2>{error.message}</h2>}
-      {countries && <Countries countries={countries} />}
+      {countries && (
+        <Countries
+          countries={filteredCountries}
+          onRemoveCountry={handleRemoveCountry}
+        />
+      )}
     </>
   );
 }
--- project-3/src/components/Country.js
+++ project-3/src/components/Country.js
@@ -1,16 +1,27 @@
 import React from "react";
+import style from "./country.module.css";
 
 const Country = (props) => {
   const { country } = props;
   const { name, flags, capital, population, area } = country;
+  const handleRemoveCountry = (name) => {
+    props.onRemoveCountry(name);
+  };
   return (
-    <article>
+    <article className={style.country}>
       <div>
-        <img src={flags.png} alt={name.common} />
+        <img src={flags.png} alt={name.common} className={style.flag} />
         <h3>Name: {name.common}</h3>
         <h3>Popilation: {population}</h3>
         <h3>Capital: {capital}</h3>
         <h3>Area:{area}</h3>
+        <button
+          className={style.btn}
+          onClick={() => {
+            handleRemoveCountry(name.common);
+          }}>
+          Remove country
+        </button>
       </div>
     </article>
   );
